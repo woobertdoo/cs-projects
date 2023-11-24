1a3a9acf3636ba84eec0f68e3f883c69cfb23f56
Jamie Harris <jamie.a.harris97@gmail.com>
2023-11-23 20:19:37 -0600

    Starting code for the letter frequency tree class

---
+++ letter-frequencies/LetterTree.cpp
@@ -0,0 +1,38 @@
+#include "LetterTree.h"
+#include <iostream>
+using namespace std;
+
+/* Private method for inserting a new node into the tree.
+ *   Searches the tree for the proper insertion point, then inserts `newNode` as a leaf in a subtree
+ * of `nodePtr`.
+ *  If the letters stored in `newNode` are found during the traversal, `newNode` is
+ * discarded and the occurence counter for the original node is incremented.
+ */
+void LetterTree::insert(TreeNode *&nodePtr, TreeNode *&newNode) {
+    if (nodePtr == nullptr) {
+        nodePtr = newNode; // If there are no nodes in the tree, insert newNode as the root
+    } else if (newNode->letters < nodePtr->letters) {
+        insert(nodePtr->left, newNode); // If the letters in newNode appear before the current
+                                        // node's letters alphabetically, search the left branch
+    } else if (newNode->letters > nodePtr->letters) {
+        insert(nodePtr->right, newNode); // If the letters in newNode appear after the current
+                                         // node's letters alphabetically, search the right branch
+    } else {
+        nodePtr->occurences++; // If the letter combination in newNode already have a node in the
+                               // tree, increment the occurences counter.
+    }
+}
+
+/* Public method for inserting a new node into a binary tree. */
+void LetterTree::insertNode(string letters) {
+    TreeNode *newNode = nullptr;
+
+    // Create a new node and store the letter distribution in it
+    // with a starting occurence count of 1
+    newNode = new TreeNode;
+    newNode->letters = letters;
+    newNode->occurences = 1;
+    newNode->left = newNode->right = nullptr;
+
+    insert(root, newNode);
+}
---
+++ letter-frequencies/LetterTree.h
@@ -0,0 +1,35 @@
+#ifndef LETTER_TREE_H
+#define LETTER_TREE_H
+
+#include "string"
+using namespace std;
+
+class LetterTree {
+private:
+    struct TreeNode {
+        string letters;
+        int occurences;
+        TreeNode *left;
+        TreeNode *right;
+    };
+
+    TreeNode *root;
+
+    void insert(TreeNode *&, TreeNode *&);
+    void destroySubTree(TreeNode *);
+    void deleteNode(int, TreeNode *&);
+    void makeDeletion(TreeNode *&);
+    void display(TreeNode *) const;
+
+public:
+    LetterTree() { root = nullptr; }
+    ~LetterTree() { destroySubTree(root); }
+
+    void insertNode(string);
+    bool searchNode(string);
+    void remove(string);
+
+    void display() const { display(root); }
+};
+
+#endif
